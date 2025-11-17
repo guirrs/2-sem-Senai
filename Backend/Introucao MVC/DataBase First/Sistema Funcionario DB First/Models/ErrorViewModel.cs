d2d267086ff88383db3be196f02afcd1e77bce4d
Guinas <guirsantos189@gmail.com>
2025-11-17 10:47:02 -0300

    fetch: DataBase First

---
+++ Backend/Introucao MVC/DataBase First/Sistema Funcionario DB First/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Sistema_Funcionario_DB_First.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
